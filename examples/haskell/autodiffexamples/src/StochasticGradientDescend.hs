e959f21cc0c064a69539d04d386eb389c18ff718
Ahmad Salim Al-Sibahi <asal@itu.dk>
2019-03-13 21:26:20 +0100

    Fix loss order

--- examples/haskell/autodiffexamples/src/StochasticGradientDescend.hs
+++ examples/haskell/autodiffexamples/src/StochasticGradientDescend.hs
@@ -9,7 +9,7 @@ sgd :: forall n a. Floating n => Rational -> n -> a -> (n -> a -> n)
                    -> (n -> a -> Double) -> Int -> ([Double], n)
 sgd lr params xs gradf lossf iters = go iters [] params
     where go :: Int -> [Double] -> n -> ([Double], n)
-          go it !losses !params | it < 0 = (losses, params)
+          go it !losses !params | it < 0 = (reverse losses, params)
                                 | otherwise = go (it - 1) (lossf params xs : losses) (update_params params)
           update_params :: n -> n
           update_params params =
